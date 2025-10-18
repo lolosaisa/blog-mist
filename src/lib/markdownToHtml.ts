55b6c7365bff9c88c4b4670bd64323ce851acace
lolosaisa <lwamaitha3924@gmail.com>
2025-10-18 10:03:58 +0300

    modified markdown to html to fetch meta data as well

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,10 @@
+//this file converts markdown to html using remark and gray-matter
 import { remark } from "remark";
 import html from "remark-html";
+import matter from "gray-matter";
 
 export default async function markdownToHtml(markdown: string) {
+  const {content} = matter(markdown);
   const result = await remark().use(html).process(markdown);
   return result.toString();
 }
