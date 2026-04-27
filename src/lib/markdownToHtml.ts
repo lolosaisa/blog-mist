2d1fd72462020f4a44e8615b08a9e4e5d19da58c
lolosaisa <lwamaitha3924@gmail.com>
2026-04-27 14:52:18 +0300

    chore: stripped out the graymatter from markdowntohtml file, it all changes the markdown string to html string

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,26 +1,7 @@
-//this file converts markdown to html using remark and gray-matter
-// import { remark } from "remark";
-// import html from "remark-html";
-// import matter from "gray-matter";
-
-// export default async function markdownToHtml(markdown: string) {
-//   const {content} = matter(markdown);
-//   const result = await remark().use(html).process(markdown);
-//   return result.toString();
-// }
 import { remark } from "remark";
 import html from "remark-html";
-import matter from "gray-matter";
 
-export async function parseMarkdown(markdown: string) {
-  const { data: frontmatter, content } = matter(markdown);
-
-  const processedContent = await remark()
-    .use(html)
-    .process(content);
-
-  return {
-    frontmatter,
-    content: processedContent.toString(),
-  };
+export async function markdownToHtml(markdown: string): Promise<string> {
+  const result = await remark().use(html).process(markdown);
+  return result.toString();
 }
