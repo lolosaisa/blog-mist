20fa55eb613aefa8dc0e9adfebb0fad79a8b4b8a
lolosaisa <lwamaitha3924@gmail.com>
2026-04-27 07:18:05 +0300

    update: deleted the github.ts to leave the Api.ts, modified gitignore and the post page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,3 +1,5 @@
+
+
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import Container from "@/app/_components/container";
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,18 +1,33 @@
-import { Post } from "@/interfaces/post";
-import fs from "fs";
-import matter from "gray-matter";
-import { join } from "path";
+ import { Post } from "@/interfaces/post";
+ import fs from "fs";
+ import matter from "gray-matter";
+ import { join } from "path";
 
-const postsDirectory = join(process.cwd(), "_posts");
+// const postsDirectory = join(process.cwd(), "_posts");
 
+// export function getPostSlugs() {
+//   return fs.readdirSync(postsDirectory);
+// }
 
-djdj
+// export function getPostBySlug(slug: string, p0: string[]) {
+//   const realSlug = slug.replace(/\.md$/, "");
+//   const fullPath = join(postsDirectory, `${realSlug}.md`);
+//   const fileContents = fs.readFileSync(fullPath, "utf8");
+//   const { data, content } = matter(fileContents);
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
-}
+//   return { ...data, slug: realSlug, content } as Post;
+// }
 
-export function getPostBySlug(slug: string, p0: string[]) {
+// export function getAllPosts(): Post[] {
+//   const slugs = getPostSlugs();
+//   const posts = slugs
+//     .map((slug) => getPostBySlug(slug))
+//     // sort posts by date in descending order
+//     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+//   return posts;
+// }
+
+export function getPostBySlug(slug: string): Post {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -23,9 +38,7 @@ export function getPostBySlug(slug: string, p0: string[]) {
 
 export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
-  const posts = slugs
+  return slugs
     .map((slug) => getPostBySlug(slug))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+    .sort((a, b) => (a.date > b.date ? -1 : 1));
 }
